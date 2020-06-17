cacd3d1d0776246f754d5f98b43b882e855cb831
waspryo <sugimoto@liddell.tokyo>
2020-06-17 11:02:17 +0900

    [WIP]Imageの使い方

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
+
+  console.log(require("./assets/icon.png"));
 
   const handlePress = () => console.log("Text Pressed");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Most components can be customized when they are created, with different parameters. These creation parameters are called props.</Text>
+      <Text style={styles.container_text} numberOfLines={1} onPress={handlePress}>Most components can be customized when they are created</Text>
+      {/* <Image source={require('./assets/icon.png')}/> */}
+      <Image
+      fadeDuration={1000}
+        blurRadius={10}
+        source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"
+        }}/>
     </SafeAreaView>
   );
 }
@@ -15,7 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4BC1B1',
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: Platform.OS === 'android' ? 25 : 0
   },
+  container_text: {
+    textAlign: "center",
+    padding: 8
+  }
 });
