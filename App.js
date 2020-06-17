7b16973c6944a3fcc646f232bb1bfe4264a21ac2
waspryo <sugimoto@liddell.tokyo>
2020-06-17 10:42:36 +0900

    [WIP]SafeAreaViewの使い方

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
 
-
+  const handlePress = () => console.log("Text Pressed");
 
   return (
-    <View style={styles.container}>
-      <Text>Hello World)</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Most components can be customized when they are created, with different parameters. These creation parameters are called props.</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#4BC1B1',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
 });
